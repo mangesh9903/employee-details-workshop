a96fb5475ca8fe0f48f30f8b6454c880278c0cd0
mangesh9903 <mangeshb9903@gmail.com>
2022-04-21 09:03:11 +0530

    Welcome to Employee Details Workshop.

---
+++ src/com/bridgelabz/controller/EmployeeHomeController.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.controller;
+
+public class EmployeeHomeController {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Details Workshop.");
+    }
+}
